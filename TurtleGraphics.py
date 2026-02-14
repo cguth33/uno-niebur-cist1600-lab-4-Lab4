06f445527e38857a66f64c8220b1df55b114831b
cguth33 <carterguthrie@unomaha.edu>
2026-02-14 01:45:16 +0000

    Lab 4 Completed

--- TurtleGraphics.py
+++ TurtleGraphics.py
@@ -1,27 +1,39 @@
 #TurtleGraphics.py
-#Name:
-#Date:
-#Assignment:
+#Name:Carter Guthrie
+#Date:2/13/2025
+#Assignment: Lab 4
 
-import turtle #needed generally but not in CodeHS
-hideturtle() #hides the default turtle in CodeHS
+import turtle
 
-def drawSquare(myTurtle, size):
-    for i in range(4):
-        myTurtle.forward(size)
-        myTurtle.right(90)
+t = turtle.Turtle()
+t.speed(3) 
+screen = turtle.Screen()
+screen.bgcolor("skyblue")
+
+'
+t.penup()
+t.goto(-400, -100) 
+t.pendown()
+
+t.fillcolor("blue") 
+t.begin_fill()      
+for _ in range(2):
+    t.forward(800)
+    t.right(90)
+    t.forward(300)
+    t.right(90)
+t.end_fill()
+
+t.penup()
+t.goto(0, 50)
+t.color("yellow")
+t.dot(100) 
 
 
-def main():
-    myTurtle = turtle.Turtle()
-    # drawPolygon(myTurtle, 5) #draws a pentagon
-    # drawPolygon(myTurtle, 8) #draws an octogon
+t.penup()
+t.goto(-100, 150)
+t.shape("classic") 
+t.color("black")
+t.stamp()         
 
-    # fillCorner(myTurtle, 2) #draws a square with top right corner filled in.
-    # fillCorner(myTurtle, 3) #draws a square bottom left corner filled in.
-
-    # squaresInSquares(myTurtle, 5) #draws 5 concentric squares
-    # squaresInSquares(myTurtle, 3) #draws 3 concentric squares
-
-
-main()
+turtle.done()
