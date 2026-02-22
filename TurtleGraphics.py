a0a1a5f2959517cda4248a4dbe733cafec76730e
cguth33 <carterguthrie@unomaha.edu>
2026-02-22 21:40:09 +0000

    Lab 4 Completed

--- TurtleGraphics.py
+++ TurtleGraphics.py
@@ -5,35 +5,44 @@
 
 import turtle
 
-t = turtle.Turtle()
-t.speed(3) 
-screen = turtle.Screen()
-screen.bgcolor("skyblue")
+# 1. First required function
+def drawPolygon(t, sides):
+    angle = 360 / sides
+    for _ in range(sides):
+        t.forward(50)
+        t.left(angle)
 
-'
-t.penup()
-t.goto(-400, -100) 
-t.pendown()
+# 2. Second required function
+def fillCorner(t, size):
+    # Draw the big square
+    for _ in range(4):
+        t.forward(size)
+        t.left(90)
+    
+    # Fill the small corner square
+    t.begin_fill()
+    for _ in range(4):
+        t.forward(size / 2)
+        t.left(90)
+    t.end_fill()
 
-t.fillcolor("blue") 
-t.begin_fill()      
-for _ in range(2):
-    t.forward(800)
-    t.right(90)
-    t.forward(300)
-    t.right(90)
-t.end_fill()
+# 3. Third required function
+def squaresInSquares(t, num):
+    size = 20
+    for _ in range(num):
+        for _ in range(4):
+            t.forward(size)
+            t.left(90)
+        size += 20 # Make the next square bigger
 
-t.penup()
-t.goto(0, 50)
-t.color("yellow")
-t.dot(100) 
+def main():
+    # Setup the turtle
+    bob = turtle.Turtle()
+    bob.speed(3)
 
+    
 
-t.penup()
-t.goto(-100, 150)
-t.shape("classic") 
-t.color("black")
-t.stamp()         
+    turtle.done()
 
-turtle.done()
+if __name__ == "__main__":
+    main()
